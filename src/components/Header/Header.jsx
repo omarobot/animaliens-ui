6f0fad257182f3413ac067004aa846615e059461
Suhag Al Amin <developersuhag@gmail.com>
2022-03-10 11:56:46 +0600

    left sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,11 @@
-import React from "react";
-import Tab from "./Tab";
-import {
-  mdiRocketOutline,
-  mdiBookOpenOutline,
-  mdiSortVariant,
-  mdiTwitter,
-  mdiTelegram,
-  mdiDiscord,
-} from "@mdi/js";
-import Icon from "@mdi/react";
-import { StartButton } from "../Account";
 import { navigate } from "gatsby";
-
 import { useBreakpoint } from "gatsby-plugin-breakpoints";
-
+import React from "react";
+import newLogo from "../../images/assets/logo_face_white.svg";
 import * as style from "./Tab.module.css";
 
-//assets
-import Logo from "../../images/assets/spacebudz.svg";
-import LogoNew from "../../images/assets/LogoNew.svg";
-import Title from "../../images/assets/title.png";
-import { Box, Link } from "@chakra-ui/layout";
+
+
 
 const isBrowser = () => typeof window !== "undefined";
 
@@ -33,7 +18,8 @@ const Header = () => {
   }, []);
 
   return (
-    <div style={{ position: "absolute", width: "100%" }}>
+    // <div style={{ position: "absolute", width: "100%", backgroundColor: '#171717' }}>
+    <div>
       <div
         id="header"
         style={{
@@ -42,6 +28,7 @@ const Header = () => {
           display: "flex",
           alignItems: "center",
           justifyContent: "space-between",
+          flexDirection: 'column'
         }}
       >
         {/* Logo */}
@@ -51,159 +38,71 @@ const Header = () => {
             alignItems: "center",
             cursor: "pointer",
             zIndex: 2,
+            textAlign: 'center',
+            marginBottom: 40
           }}
           onClick={() => {
             navigate("/");
             window.scrollTo(0, 0);
           }}
         >
-          <img draggable={false} width={!matches.md ? 55 : 45} src={LogoNew} />
-          {!matches.md && (
-            <>
-              <Box w={4} />
-              <Box
-                fontFamily={"'Nunito', sans-serif;"}
-                fontSize={22}
-                color={
-                  win && win.location.href.split("/")[3] ? "#8064F4" : "white"
-                }
-                fontWeight={700}
-              >
-                SpaceBudz
-              </Box>
-            </>
-          )}
+          <img draggable={false} width={!matches.md ? 120 : 45} src={newLogo} />
         </div>
 
-        {/* Right Side */}
+{/* menu */}
+      <div>
         <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            zIndex: 1,
-            height: !matches.md ? "65px" : "45px",
-            background: "white",
-            borderRadius: "40px",
-            padding: !matches.md ? "10px 40px" : "5px 10px",
-            marginRight: !matches.md && -20,
-          }}
-        >
-          <Tab
-            icon={mdiRocketOutline}
-            onClick={() => {
-              navigate("/explore");
-              window.scrollTo(0, 0);
-            }}
-          >
-            Explore
-          </Tab>
-          <Box w={7} />
-          <Tab
-            icon={mdiBookOpenOutline}
-            menu={(onClose) => (
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                flexDir="column"
-                p="2"
-              >
-                <Box
                   color="gray.600"
                   className={style.tabItem}
+                  style={{fontWeight: 500, fontSize:16, marginBottom:10, textTransform: 'uppercase'}}
                   onClick={() => {
-                    onClose();
+                    navigate("/explore");
+                  }}
+                >
+                  MarketPlace
+                </div>
+        <div
+                  color="gray.600"
+                  className={style.tabItem}
+                  style={{fontWeight: 500, fontSize:16, marginBottom:10, textTransform: 'uppercase'}}
+                  onClick={() => {
                     navigate("/tutorial");
                   }}
                 >
-                  How It Works
-                </Box>
-                <Box w="120%" h="1px" my="2.5" background="gray.200" />
-                <Box
+                  RoadMap
+                </div>
+        <div
                   color="gray.600"
                   className={style.tabItem}
+                  style={{fontWeight: 500, fontSize:16, marginBottom:10, textTransform: 'uppercase'}}
                   onClick={() => {
-                    onClose();
-                    navigate("/FAQ");
+                    navigate("/tutorial");
                   }}
                 >
-                  FAQ
-                </Box>
-              </Box>
-            )}
-          >
-            Guide
-          </Tab>
-          <Box w={7} />
-          <Tab
-            icon={mdiSortVariant}
-            menu={(onClose) => (
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                flexDir="column"
-                p="2"
-              >
-                <Box className={style.tabItemTitle}>Social media</Box>
-                <Box w="120%" h="1px" my="2.5" background="gray.200" />
-                <Box
-                  w="full"
-                  textAlign="left"
+                  Rarities
+                </div>
+        <div
                   color="gray.600"
                   className={style.tabItem}
-                  onClick={() =>
-                    window.open("https://twitter.com/spacebudzNFT")
-                  }
-                  style={{ display: "flex", alignItems: "center" }}
-                >
-                  <Icon path={mdiTwitter} size={0.7} />{" "}
-                  <span style={{ marginLeft: 6 }}>Twitter</span>
-                </Box>
-                <Box h="2" />
-                <Box
-                  w="full"
-                  textAlign="left"
-                  color="gray.600"
-                  className={style.tabItem}
-                  onClick={() => window.open("https://t.me/spacebudz")}
-                  style={{ display: "flex", alignItems: "center" }}
-                >
-                  <Icon path={mdiTelegram} size={0.7} />{" "}
-                  <span style={{ marginLeft: 6 }}>Telegram</span>
-                </Box>
-                <Box h="2" />
-
-                <Box
-                  w="full"
-                  textAlign="left"
-                  color="gray.600"
-                  className={style.tabItem}
-                  onClick={() => window.open("https://discord.gg/spacebudz")}
-                  style={{ display: "flex", alignItems: "center" }}
-                >
-                  <Icon path={mdiDiscord} size={0.7} />{" "}
-                  <span style={{ marginLeft: 6 }}>Discord</span>
-                </Box>
-                <Box w="120%" h="1px" my="2.5" background="gray.200" />
-                <Box
-                  color="gray.600"
-                  className={style.tabItem}
+                  style={{fontWeight: 500, fontSize:16, marginBottom:10, textTransform: 'uppercase'}}
                   onClick={() => {
-                    onClose();
                     navigate("/about");
                   }}
                 >
-                  About
-                </Box>
-              </Box>
-            )}
-          >
-            More
-          </Tab>
-          <Box w={7} />
-          <StartButton />
+                  About Us
+                </div>
+        <div
+                  color="gray.600"
+                  className={style.tabItem}
+                  style={{fontWeight: 500, fontSize:16, marginBottom:10, textTransform: 'uppercase'}}
+                  onClick={() => {
+                    navigate("/tutorial");
+                  }}
+                >
+                  How to Buy
+                </div>
         </div>
+
       </div>
     </div>
   );
--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { StoreProvider } from "easy-peasy";
 import store from "../store";
-import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Header from "../components/Header/Header";
 
 const Layout = (props) => {
   return (
     <StoreProvider store={store}>
       <div
+        className="layout"
         style={{
           width: "100%",
           display: "flex",
-          alignItems: "center",
+          // alignItems: "center",
           justifyContent: "center",
         }}
       >
-        <div style={{ width: "100%" }}>
+        <div
+          style={{
+            width: "15%",
+            backgroundColor: "#171717",
+            color: "#fff",
+          }}
+        >
           <Header />
+        </div>
+        <div style={{ width: "85%" }}>
           {props.children}
           <Footer />
         </div>
