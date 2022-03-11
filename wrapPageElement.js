9c2550119d97e234cfe16297ee0520db213a6c5e
Suhag Al Amin <developersuhag@gmail.com>
2022-03-11 11:53:19 +0600

    bg color

--- wrapPageElement.js
+++ wrapPageElement.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "./src/templates/layout";
 import { Buffer } from "buffer";
+import "./src/styles/global.css";
 if (typeof window !== "undefined") window.Buffer = Buffer;
 
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
