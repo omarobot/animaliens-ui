6c90b736a53d8ad0c8dde0eb42763e786267bd02
Suhag Al Amin <developersuhag@gmail.com>
2022-05-30 14:16:43 +0600

    new raffle

--- src/components/Header/Sidebar.jsx
+++ src/components/Header/Sidebar.jsx
@@ -1,44 +1,43 @@
 import {
-    Box,
-    CloseButton, Drawer,
-    DrawerContent, Flex,
-    HStack, IconButton, Menu,
-    MenuButton, MenuList, useDisclosure
-} from '@chakra-ui/react';
+  Box,
+  CloseButton,
+  Drawer,
+  DrawerContent,
+  Flex,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { Link, navigate } from "gatsby";
 import { useBreakpoint } from "gatsby-plugin-breakpoints";
-import React from 'react';
-import {
-    FiMenu
-} from 'react-icons/fi';
+import React from "react";
+import { FiMenu } from "react-icons/fi";
 // import * as style from "../../components/Header/Tab.module.css";
-import newLogo from '../../images/assets/logo_face_white.svg';
-import * as style from '../Header/sidebar.module.css'
-
-
-
+import newLogo from "../../images/assets/logo_face_white.svg";
+import * as style from "../Header/sidebar.module.css";
 
 const LinkItems = [
-  {name: '          ', link: '/'},
-  { name: 'Store', link: '/explore' },
-  { name: 'Lotto', link: '/tutorial'  },
+  { name: "          ", link: "/" },
+  { name: "Store", link: "/explore" },
+  { name: "Lotto", link: "/tutorial" },
   // { name: 'Rarities', link: '/tutorial'},
   // { name: 'Marketplace', link: '/tutorial'  },
 
-  { name: 'About Us', link: '/about'},
+  { name: "About Us", link: "/about" },
+  { name: "New Raffle", link: "/new-raffle" },
   // { name: 'How to Buy', link: '/tutorial'  },
 ];
 
-
-export default function SidebarWithHeader({
-  children,
-}) {
+export default function SidebarWithHeader({ children }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box minH="100vh" >
+    <Box minH="100vh">
       <SidebarContent
         onClose={() => onClose}
-        display={{ base: 'none', md: 'block' }}
+        display={{ base: "none", md: "block" }}
       />
       <Drawer
         autoFocus={false}
@@ -47,7 +46,8 @@ export default function SidebarWithHeader({
         onClose={onClose}
         returnFocusOnClose={false}
         onOverlayClick={onClose}
-        size="full">
+        size="full"
+      >
         <DrawerContent>
           <SidebarContent onClose={onClose} />
         </DrawerContent>
@@ -61,48 +61,61 @@ export default function SidebarWithHeader({
   );
 }
 
-
 const SidebarContent = ({ onClose, ...rest }) => {
-    const matches = useBreakpoint();
+  const matches = useBreakpoint();
   return (
     <Box
       transition="3s ease"
       bg="#171717"
-      w={{ base: 'full', md: 60 }}
+      w={{ base: "full", md: 60 }}
       pos="fixed"
       h="full"
-      {...rest}>
-      <Flex  alignItems="center" mx="8" justifyContent="space-around">
-
-             {/* Logo */}
-             <div
+      {...rest}
+    >
+      <Flex alignItems="center" mx="8" justifyContent="space-around">
+        {/* Logo */}
+        <div
           style={{
-
             display: "flex",
             alignItems: "center",
             cursor: "pointer",
             zIndex: 2,
-            textAlign: 'center',
+            textAlign: "center",
           }}
           onClick={() => {
             navigate("/");
             window.scrollTo(0, 0);
           }}
         >
-          <img draggable={false} width={!matches.md ? 150 : 120}  src={newLogo} />
+          <img
+            draggable={false}
+            width={!matches.md ? 150 : 120}
+            src={newLogo}
+          />
         </div>
-        <CloseButton display={{ base: 'flex', md: 'none', }} style={{color: '#fff'}} onClick={onClose} />
+        <CloseButton
+          display={{ base: "flex", md: "none" }}
+          style={{ color: "#fff" }}
+          onClick={onClose}
+        />
       </Flex>
-          <div style={{marginTop:20,}}>
-          {LinkItems.map((link) => (
-        // <NavItem key={link.name} link={link.link}>
-        //   {link.name}
-        // </NavItem>
-        <div className={style.allLinks}>
-          <Link className={style.linkItem} activeClassName={style.active} to={link.link} key={link.name}>{link.name}</Link>
-        </div>
-      ))}
+      <div style={{ marginTop: 20 }}>
+        {LinkItems.map((link) => (
+          // <NavItem key={link.name} link={link.link}>
+          //   {link.name}
+          // </NavItem>
+          <div className={style.allLinks}>
+            <Link
+              className={style.linkItem}
+              activeClassName={style.active}
+              to={link.link}
+              key={link.name}
+            >
+              {link.name}
+            </Link>
           </div>
+        ))}
+      </div>
     </Box>
   );
 };
@@ -111,52 +124,57 @@ const NavItem = ({ link, children, ...rest }) => {
   return (
     // <div
     <Link
-    // color="gray.600"
-    // className={style.tabItem}
-    style={{fontWeight: 500, fontSize:24, marginBottom:10, textTransform: 'uppercase'}}
-    // onClick={() => {
-    //   navigate(`${link}`);
-    // }}
-    to={link}
-    className="link-item"
-    activeClassName="active"
-  >
+      // color="gray.600"
+      // className={style.tabItem}
+      style={{
+        fontWeight: 500,
+        fontSize: 24,
+        marginBottom: 10,
+        textTransform: "uppercase",
+      }}
+      // onClick={() => {
+      //   navigate(`${link}`);
+      // }}
+      to={link}
+      className="link-item"
+      activeClassName="active"
+    >
       <Flex
-      style={{color: '#fff'}}
+        style={{ color: "#fff" }}
         align="center"
         p="1"
         mx="8"
         cursor="pointer"
-        {...rest}>
-        
+        {...rest}
+      >
         {children}
       </Flex>
     </Link>
   );
 };
 
-
 const MobileNav = ({ onOpen, ...rest }) => {
-    const matches = useBreakpoint();
+  const matches = useBreakpoint();
   return (
     <Flex
       ml={{ base: 0, md: 60 }}
       px={{ base: 4, md: 4 }}
       height="20"
       alignItems="center"
-      justifyContent={{ base: 'space-between', md: 'flex-end' }}
-      {...rest}>
+      justifyContent={{ base: "space-between", md: "flex-end" }}
+      {...rest}
+    >
       <IconButton
-      style={{color: '#fff'}}
-        display={{ base: 'flex', md: 'none' }}
+        style={{ color: "#fff" }}
+        display={{ base: "flex", md: "none" }}
         onClick={onOpen}
         variant="outline"
         aria-label="open menu"
         icon={<FiMenu />}
       />
 
-           {/* Logo */}
-           {/* <div
+      {/* Logo */}
+      {/* <div
           style={{
             display: "flex",
             alignItems: "center",
@@ -173,18 +191,11 @@ const MobileNav = ({ onOpen, ...rest }) => {
           <img draggable={false} src={newLogo} />
         </div> */}
 
-      <HStack spacing={{ base: '0', md: '6' }}>
-        <Flex alignItems={'center'}>
+      <HStack spacing={{ base: "0", md: "6" }}>
+        <Flex alignItems={"center"}>
           <Menu>
-            <MenuButton
-            style={{color: '#fff'}}
-              py={2}
-              >
-             
-            </MenuButton>
-            <MenuList
-              bg="#fff"
-              >
+            <MenuButton style={{ color: "#fff" }} py={2}></MenuButton>
+            <MenuList bg="#fff">
               {/* <MenuItem>Profile</MenuItem>
               <MenuItem>Settings</MenuItem>
               <MenuItem>Billing</MenuItem>
