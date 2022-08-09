e4b3cfa9c3092b2021a98c989d4a5f4b8b9973c8
Suhag Al Amin <developersuhag@gmail.com>
2022-08-09 12:05:03 +0600

    upload image to firebase and get url

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,6 +2,8 @@
 import { getFirestore } from "@firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +23,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+export const storage = getStorage(app);
 export const db = getFirestore(app);
--- src/pages/new-raffle.js
+++ src/pages/new-raffle.js
@@ -16,22 +16,70 @@ import React, { useState } from "react";
 import Metadata from "../components/Metadata";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { db } from "../firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { db, storage } from "../firebase-config";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { useEffect } from "react";
+import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
 
 const NewRaffle = () => {
+  // states
   const [startDate, setStartDate] = useState(new Date());
   const [users, setUsers] = useState([]);
+  const [raffleImg, setRaffleImg] = useState(null);
+  const [url, setURL] = useState("");
+
+  // firebase connection
+  // upload image to firebase storage
+  const storage = getStorage();
+  const imagesRef = ref(storage, `rafflesImages/${raffleImg?.name}`);
+
   const userCollection = collection(db, "users");
+
+  const raffleCollection = collection(db, "raffles");
+  console.log(raffleImg);
+
+  const createUser = async () => {
+    const docRef = await addDoc(userCollection, {
+      firstname: "Suhag",
+      lastname: "Al Amin",
+    });
+    console.log("Document written with ID: ", docRef.id);
+  };
+  // createUser();
 
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(userCollection);
-      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getUsers();
   }, []);
+
+  // handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // upload image to firebase storage
+    if (raffleImg === null) {
+      alert("Please select an image");
+      return;
+    } else {
+      uploadBytes(imagesRef, raffleImg)
+        .then((snapshot) => {
+          console.log("Uploaded a blob or file!");
+        })
+        .then(() => {
+          getDownloadURL(imagesRef).then((url) => {
+            console.log(url);
+            setURL(url);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
     <>
       <Metadata
@@ -46,39 +94,45 @@ const NewRaffle = () => {
             Add new raffle
           </Heading>
           <div>
-            <FormControl sx={{ my: 2 }}>
-              <FormLabel htmlFor="title">Title</FormLabel>
-              <Input sx={{ _focus: "none" }} id="title" placeholder="Title" />
-            </FormControl>
-            <FormControl sx={{ my: 2 }}>
-              <FormLabel htmlFor="description">Description</FormLabel>
-              <Textarea sx={{ _focus: "none" }} placeholder="Description" />
-            </FormControl>
-            <FormControl sx={{ my: 2 }}>
-              <FormLabel htmlFor="winners"># of winners</FormLabel>
-              <NumberInput max={50} min={0}>
-                <NumberInputField sx={{ _focus: "none" }} id="winners" />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-            <FormControl sx={{ my: 2 }}>
-              <FormLabel htmlFor="date">Date raffle ends</FormLabel>
-              <DatePicker
-                className="date-picker"
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-              />
-            </FormControl>
-            <FormControl sx={{ my: 2 }}>
-              <FormLabel htmlFor="image">Image</FormLabel>
-              <input type="file" id="image" />
-            </FormControl>
-            <Button mt={4} colorScheme="green" type="submit">
-              Submit
-            </Button>
+            <form onSubmit={handleSubmit}>
+              <FormControl sx={{ my: 2 }}>
+                <FormLabel htmlFor="title">Title</FormLabel>
+                <Input sx={{ _focus: "none" }} id="title" placeholder="Title" />
+              </FormControl>
+              <FormControl sx={{ my: 2 }}>
+                <FormLabel htmlFor="description">Description</FormLabel>
+                <Textarea sx={{ _focus: "none" }} placeholder="Description" />
+              </FormControl>
+              <FormControl sx={{ my: 2 }}>
+                <FormLabel htmlFor="winners"># of winners</FormLabel>
+                <NumberInput max={50} min={0}>
+                  <NumberInputField sx={{ _focus: "none" }} id="winners" />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
+              <FormControl sx={{ my: 2 }}>
+                <FormLabel htmlFor="date">Date raffle ends</FormLabel>
+                <DatePicker
+                  className="date-picker"
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </FormControl>
+              <FormControl sx={{ my: 2 }}>
+                <FormLabel htmlFor="image">Image</FormLabel>
+                <input
+                  onChange={(e) => setRaffleImg(e.target.files[0])}
+                  type="file"
+                  id="image"
+                />
+              </FormControl>
+              <Button mt={4} colorScheme="green" type="submit">
+                Submit
+              </Button>
+            </form>
           </div>
         </Container>
       </main>
